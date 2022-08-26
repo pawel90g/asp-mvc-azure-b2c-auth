c68d4ca8fc3ca84d803967c25d3fee71caddb3bb
Pawel Garbacik <Pawel.Garbacik@equiniti.com>
2022-08-26 07:34:48 +0200

    Init commit. SPA in MVC. SPA access with authorization

---
+++ ASP.NET React App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_React_App.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
